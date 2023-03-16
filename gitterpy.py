1b2619bdf4b73a8c7e5988fa59a5abb7dce72dce
Sounder Rajendran <sounderajendran@gmail.com>
2023-03-16 13:55:52 -0400

    py commit @ 3/16/2023-1:55:52 PM

--- gitterpy.py
+++ gitterpy.py
@@ -1,6 +1,9 @@
+# The following packages are called 'standard library packages' as they are included with python
+# Have a look at https://docs.python.org/3/library/index.html for info on standard lib packages.
 import os
 import pip
 import sys
+import platform
 
 # Define a function to check whether a required package is installed
 # If it isn't installed, use pip to install it
@@ -17,7 +20,6 @@ import_or_install('pytest')
 
 # Import necessary modules
 from git import Repo
-from sys import platform
 from datetime import datetime
 
 # Assign the current date and time to the 'timestamp' variable
@@ -30,6 +32,9 @@ PATH_OF_GIT_REPO = r'.git'
 # Create a commit message that includes the current timestamp
 COMMIT_MESSAGE = 'py commit @ ' + timestamp.strftime('%x-%X')
 
+# lambda func to ret splitting lines, def arg is 0
+retLine = lambda err=0 : '\n'+'!'*100+'\n' if err else '\n'+'='*100+'\n'
+
 # Define a function to push changes to the remote Git repository
 def git_push():
     try:
@@ -40,20 +45,26 @@ def git_push():
         # Use GitPython to push changes to the remote repository
         origin = repo.remote(name='origin')
         origin.push()
-    except:
+    except Exception:
         # Print an error message if an error occurs during the push
-        print('Some error occured while pushing the code. Contact the instructor')
+        print(retLine(1) + 'Some error occured while pushing the code.\nContact the instructor with a screenshot of message\n' + Exception + retLine(1))
+        
+    else:
+        print("Yay! Successfully uploaded" + retLine())
 
 # Check the value of the first argument passed to the script
 if sys.argv[1]=='test':
 
     # If the platform is Linux or macOS, use 'python3 -m pytest' to run pytest
-    if platform == "linux" or platform == "linux2" or platform == "darwin":
+    if platform.system() == "Linux" or platform.system() == "Darwin":
         os.system("python3 -m pytest")
     
     # If the platform is Windows, use 'pytest' to run pytest
-    elif platform == "win32":
+    elif platform.system() == "Windows":
         os.system("pytest")
+
+    else:
+        raise Exception(retLine(1) + "You must be running some fancy OS. Contact the instructor with a screenshot of message\n" + platform.system() + retLine(1))
 
 # If the first argument is 'upload', call the 'git_push()' function to push changes to the remote repository
 elif sys.argv[1]=='upload':
