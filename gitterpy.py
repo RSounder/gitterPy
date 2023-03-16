7b86c6a6b20ff8502df6581ea21c24780bbd05f1
Sounder Rajendran <sounderajendran@gmail.com>
2023-03-16 15:15:39 -0400

    py commit @ 3/16/2023-3:15:39 PM

--- gitterpy.py
+++ gitterpy.py
@@ -19,12 +19,9 @@ def import_or_install(package):
 # If not, install them
 import_or_install('GitPython')
 import_or_install('pytest')
-import_or_install('colorama')
 
 # Import after checking installation
 from git import Repo
-from colorama import init, Fore, Back, Style
-init(autoreset=True)
 
 # Assign the current date and time to the 'timestamp' variable
 timestamp = datetime.datetime.now()
@@ -36,8 +33,12 @@ PATH_OF_GIT_REPO = r'.git'
 # Create a commit message that includes the current timestamp
 COMMIT_MESSAGE = 'py commit @ ' + timestamp.strftime('%x-%X')
 
-# lambda func to ret splitting lines, def arg is 0
-retLine = lambda err=0 : '\n'+'!'*100+'\n' if err else '\n'+'='*100+'\n'
+# print with colors for CLI outputs 
+def wrapOutput(errFlag, str):
+    if errFlag:
+        print('!'*100 + '\n\033[38;5;197m' + str + '\033[0;0m\n' + '!'*100)
+    else:
+        print('='*100 + '\n\033[38;5;119m' + str + '\033[0;0m\n' + '='*100)
 
 def run_pytest(cmd):
     try:
@@ -45,9 +46,7 @@ def run_pytest(cmd):
         subprocess.run(cmd)
     except subprocess.CalledProcessError:
         # Print an error message if an error occurs during the pytest run
-        print(retLine(1) + Style.BRIGHT + Back.YELLOW + Fore.RED + \
-              'Some error occured while running pytest. Contact the instructor with a screenshot of message\033[0;0m\n' + \
-                traceback.format_exc() + retLine(1))
+        wrapOutput(1, ' Some error occured while running pytest. Contact the instructor with a screenshot of message\n' + traceback.format_exc())
 
 # Define a function to push changes to the remote Git repository
 def git_push():
@@ -62,11 +61,9 @@ def git_push():
         origin.push()
     except Exception:
         # Print an error message if an error occurs during the push
-        print(retLine(1) + Style.BRIGHT + Back.YELLOW + Fore.RED + \
-              'Some error occured while pushing the code.\nContact the instructor with a screenshot of message\n' + \
-                Exception + retLine(1))
+        wrapOutput(1, 'Some error occured while pushing the code.\nContact the instructor with a screenshot of message\n' + Exception)
     else:
-        print('\033[2;31;43m Yay! Successfully uploaded \033[0;0m' + retLine())
+        wrapOutput(0, 'Yay! Successfully uploaded')
 
 # Check the value of the first argument passed to the script
 if sys.argv[1]=='test':
@@ -80,9 +77,7 @@ if sys.argv[1]=='test':
         run_pytest('pytest')
 
     else:
-        print(retLine(1) + Style.BRIGHT + Back.YELLOW + Fore.RED + \
-                'You must be running some fancy OS. Contact the instructor with a screenshot of message\n' + \
-                    platform.system() + retLine(1))
+        wrapOutput(1, 'You must be running some fancy OS. Contact the instructor with a screenshot of message\n' + platform.system())
 
 # If the first argument is 'upload', call the 'git_push()' function to push changes to the remote repository
 elif sys.argv[1]=='upload':
