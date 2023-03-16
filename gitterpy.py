83162c83c97b4fdc1b7369b2a553ed2fccbe7e96
Sounder Rajendran <sounderajendran@gmail.com>
2023-03-16 14:34:40 -0400

    py commit @ 3/16/2023-2:34:40 PM

--- gitterpy.py
+++ gitterpy.py
@@ -1,10 +1,11 @@
 # The following packages are called 'standard library packages' as they are included with python
 # Have a look at https://docs.python.org/3/library/index.html for info on standard lib packages.
-import os
 import pip
 import sys
 import platform
-import warnings
+import datetime
+import subprocess
+import traceback
 
 # Define a function to check whether a required package is installed
 # If it isn't installed, use pip to install it
@@ -19,15 +20,11 @@ def import_or_install(package):
 import_or_install('GitPython')
 import_or_install('pytest')
 
-# Treat warnings as errors
-warnings.filterwarnings("error")
-
-# Import necessary modules
+# Import after checking installation
 from git import Repo
-from datetime import datetime
 
 # Assign the current date and time to the 'timestamp' variable
-timestamp = datetime.now()
+timestamp = datetime.datetime.now()
 
 # Set the path of the Git repository directory
 # By default it is the same directory as your .git folder
@@ -38,6 +35,16 @@ COMMIT_MESSAGE = 'py commit @ ' + timestamp.strftime('%x-%X')
 
 # lambda func to ret splitting lines, def arg is 0
 retLine = lambda err=0 : '\n'+'!'*100+'\n' if err else '\n'+'='*100+'\n'
+
+def run_pytest(cmd):
+    try:
+        # Use subprocess to run the pytest command
+        subprocess.run(cmd)
+    except subprocess.CalledProcessError:
+        # Print an error message if an error occurs during the pytest run
+        print(retLine(1) + \
+              'Some error occured while running pytest. Contact the instructor with a screenshot of message\n' + \
+                traceback.format_exc() + retLine(1))
 
 # Define a function to push changes to the remote Git repository
 def git_push():
@@ -51,24 +58,28 @@ def git_push():
         origin.push()
     except Exception:
         # Print an error message if an error occurs during the push
-        print(retLine(1) + 'Some error occured while pushing the code.\nContact the instructor with a screenshot of message\n' + Exception + retLine(1))
+        print(retLine(1) + \
+              'Some error occured while pushing the code.\nContact the instructor with a screenshot of message\n' + \
+                Exception + retLine(1))
         
     else:
-        print("Yay! Successfully uploaded" + retLine())
+        print('Yay! Successfully uploaded' + retLine())
 
 # Check the value of the first argument passed to the script
 if sys.argv[1]=='test':
 
     # If the platform is Linux or macOS, use 'python3 -m pytest' to run pytest
-    if platform.system() == "Linux" or platform.system() == "Darwin":
-        os.system("python3 -m pytest")
+    if platform.system() == 'Linux' or platform.system() == 'Darwin':
+        run_pytest('python3 -m pytest')
     
     # If the platform is Windows, use 'pytest' to run pytest
-    elif platform.system() == "Windows":
-        os.system("pytest")
+    elif platform.system() == 'Windows':
+        run_pytest('pytest')
 
     else:
-        raise Exception(retLine(1) + "You must be running some fancy OS. Contact the instructor with a screenshot of message\n" + platform.system() + retLine(1))
+        raise Exception(retLine(1) + \
+                        'You must be running some fancy OS. Contact the instructor with a screenshot of message\n' + \
+                            platform.system() + retLine(1))
 
 # If the first argument is 'upload', call the 'git_push()' function to push changes to the remote repository
 elif sys.argv[1]=='upload':
